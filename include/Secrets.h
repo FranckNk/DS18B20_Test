35b1d674453de8c93a5f0527bfa91040f4fb39aa
FranckNk <nkeuboufranck@gmail.com>
2022-03-28 14:28:23 -0300

    first commit

---
+++ include/Secrets.h
@@ -0,0 +1,48 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+
+/*
+#define WIFI_SECRET_SSID "FibreOP532"
+#define WIFI_SECRET_PASS "9PXPE66PM6XM55M8"
+#define WIFI_SECRET_USER ""
+*/
+
+
+
+#define WIFI_SECRET_SSID "IDO-OBJECTS"
+#define WIFI_SECRET_PASS "42Bidules!"
+
+
+
+/*
+#define WIFI_SECRET_SSID "CCNB-IOT-WRLS"
+#define WIFI_SECRET_USER "ba-ido-01"
+#define WIFI_SECRET_PASS "iotwifi2020"
+*/
+
+/*
+#define WIFI_SECRET_SSID "MotorolaGPower"
+#define WIFI_SECRET_PASS "Detergent!"
+*/
+
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN  "kcSFdhW76lGziXcgIdRF"
+#define SECRET_DEVICE_ID "73ea3910-aebb-11ec-9235-d318ce92df6e"
+
+
+
